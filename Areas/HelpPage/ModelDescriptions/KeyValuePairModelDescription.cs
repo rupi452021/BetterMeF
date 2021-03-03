7c23f26369686ca1e5d7068c047203814064b5e3
rupi452021 <77400665+rupi452021@users.noreply.github.com>
2021-03-03 12:45:08 +0200

    Initial commit

---
+++ Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace BetterMe.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
